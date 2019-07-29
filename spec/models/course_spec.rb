c8defe032263f8e747f058e71d8db04cb27f5b56
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-28 20:50:09 -0600

    Add model test for student sorting by grade

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Course, type: :model do
     it {should have_many :student_grades}
     it {should have_many(:students).through(:student_grades)}
   end
+
+  describe "Class Methods" do
+    before :each do
+      @calculus = Course.create(name: "Calculus")
+      @tyler = Student.create(name: "Tyler B")
+      @bob = Student.create(name: "Bob Bert")
+      @steve = Student.create(name: "Steve Beans")
+      @tyler.student_grades.create(course: @calculus, grade: 3.0)
+      @bob.student_grades.create(course: @calculus, grade: 2.5)
+      @steve.student_grades.create(course: @calculus, grade: 1.0)
+    end
+
+    it '.sorted_by_grade' do
+      students = @calculus.sorted_by_grade
+      sorted = students.map do |student|
+        student.name
+      end
+      expect(sorted).to eq([@tyler.name, @bob.name, @steve.name])
+    end
+
+  end
 end
